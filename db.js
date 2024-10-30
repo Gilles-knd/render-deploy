00f364f9f9bdfbe852b8953a36ee8a1b9138e2ff
Kani <gkounkoud@myges.fr>
2024-10-30 12:11:00 +0100

    db init

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  process.env.DB_NAME,
+
   {
     dialect: 'postgres',
     dialectOptions: {
